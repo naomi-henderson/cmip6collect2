ff0632917f7001569ee7abcd083c823546676f22
naomi-henderson <31460695+naomi-henderson@users.noreply.github.com>
2020-12-19 15:23:41 -0500

    testing

--- myconfig.py
+++ myconfig.py
@@ -16,7 +16,6 @@ drop_keys   = ['version','ncfile']
 target_keys = ['activity_id','institution_id','source_id','experiment_id','member_id','table_id','variable_id','grid_label']
 
 target_format = '%(' + ')s/%('.join(target_keys) + ')s'
-
 target_prefix = 'gs://cmip6/'
 
 node_pref = {'esgf-data1.llnl.gov':0,'esgf-data2.llnl.gov':1,'aims3.llnl.gov':2,'esgdata.gfdl.noaa.gov':3,'esgf-data.ucar.edu':4, 'dpesgf03.nccs.nasa.gov':5,'crd-esgf-drc.ec.gc.ca':6, 'cmip.bcc.cma.cn':10, 'cmip.dess.tsinghua.edu.cn':10, 'cmip.fio.org.cn':10, 'dist.nmlab.snu.ac.kr':10, 'esg-cccr.tropmet.res.in':10, 'esg-dn1.nsc.liu.se':10, 'esg-dn2.nsc.liu.se':10, 'esg.camscma.cn':10, 'esg.lasg.ac.cn':10, 'esg1.umr-cnrm.fr':10, 'esgf-cnr.hpc.cineca.it':10, 'esgf-data2.diasjp.net':10, 'esgf-data3.ceda.ac.uk':10, 'esgf-data3.diasjp.net':10, 'esgf-nimscmip6.apcc21.org':10, 'esgf-node2.cmcc.it':10, 'esgf.bsc.es':10, 'esgf.dwd.de':10, 'esgf.ichec.ie':10, 'esgf.nci.org.au':10, 'esgf.rcec.sinica.edu.tw':10, 'esgf3.dkrz.de':10, 'noresg.nird.sigma2.no':10, 'polaris.pknu.ac.kr':10, 'vesg.ipsl.upmc.fr':10}
