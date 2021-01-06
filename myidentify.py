6898819b600dbc16fefff7912e1b644cb866e511
naomi-henderson <31460695+naomi-henderson@users.noreply.github.com>
2021-01-06 00:34:58 -0500

    worked on the tracking2version exceptions

--- myidentify.py
+++ myidentify.py
@@ -1,71 +1,82 @@
 import requests
 import zarr
 import fsspec
+import warnings
+warnings.simplefilter('always', UserWarning)
 
-def tracking2version(zstore,method='fsspec'):
-
+def id2jdict(id):
     client = requests.session()
     baseurl =  'http://hdl.handle.net/api/handles/'
-    query1 = '?type=IS_PART_OF'
-    query2 = '?type=VERSION_NUMBER'
 
-    # get the `netcdf_tracking_ids` from the zstore metadata
-    if method == 'fsspec':
-        mapper = fsspec.get_mapper(zstore)
-    else:
-        mapper = zstore        
-    group = zarr.open_consolidated(mapper)     
-    tracking_ids = group.attrs['tracking_id']
+    url = f'{baseurl}{id[4:]}'
+    r = client.get(url)
+    r.raise_for_status()
+    dict2 = r.json()
+    dtype = [s['type'] for s in dict2['values']]
+    ddata = [s['data']['value'] for s in dict2['values']]
+    return dict(zip(dtype,ddata))
+
+def gsurl2tracks(gsurl):
+    mapper = fsspec.get_mapper(gsurl)
+    group = zarr.open_consolidated(mapper)
+    tracking_ids = group.attrs['tracking_id'].split('\n')
+
+    if len(tracking_ids) != len(set(tracking_ids)):
+        msg = f'\nnetcdf file tracking_ids are NOT UNIQUE!\n{tracking_ids}\n'
+        warnings.warn("\n" + msg)
+
+    return group.attrs['tracking_id']
+
+def _get_dsid(tracks):
+    ids = []
+    for track in tracks.split('\n'):
+        jdict = id2jdict(track)
+        ds_tracking_id = jdict['IS_PART_OF']
+        ids += [ds_tracking_id]
         
-    # query the dataset handler to obtain `dataset_tracking_id` and `version`
-    versions = []
-    datasets = []
-    for file_tracking_id in tracking_ids.split('\n')[0:1]:
-        url = baseurl+file_tracking_id[4:]+query1
-        r = client.get(url)
-        try:
-            r.raise_for_status()
-            dataset_tracking_id = r.json()['values'][0]['data']['value']
-        except:
-            #assert False , f"{zstore}: {tracking_ids} , tracking_id not found"
-            dataset_tracking_id = 'unknown'
-            print(f"{zstore}: {tracking_ids}, {url} , dataset tracking_id not found")
-            
-        datasets += [dataset_tracking_id]
-        if ';' in dataset_tracking_id:
-            # multiple dataset_ids erroneously reported
-            dtracks = dataset_tracking_id.split(';')
-            vs = []
-            for dtrack in dtracks:
-                url2 = baseurl + dtrack[4:] + query2
-                r = client.get(url2)
-                try:
-                    r.raise_for_status()
-                    v = r.json()['values'][0]['data']['value']
-                except:
-                    #assert False,f"{zstore}: {file_tracking_id} , tracking_id not found"
-                    print(f"{zstore}: {tracking_ids}, {url2} , dataset tracking_id not found")
-                    v = 'unknown'
-                
-                vs += [v]
-            v = sorted(vs)[-1]    
-        else:
-            url2 = baseurl + dataset_tracking_id[4:] + query2
-            r = client.get(url2)
-            try:
-                r.raise_for_status()
-                v = r.json()['values'][0]['data']['value']
-            except:
-                #assert False,f"{zstore}: {dtrack} , tracking_id not found"
-                print(f"{zstore}: {tracking_ids}, {url2} , dataset tracking_id not found")
-                v = 'unknown'
-            
-        versions += [v]
+    if len(set(ids)) > 1:
+        warnings.warn(f'\n\nmultiple dataset_ids correspond to the dataset tracking_ids!\n{ids}\n')
 
-    version_id = list(set(versions))
-    dataset_id = list(set(datasets))
+    return ds_tracking_id
 
-    assert len(version_id)==1
+def _get_dsdict(tracks):
+    ds_tracking_id = _get_dsid(tracks)
+    jdict_ds = id2jdict(ds_tracking_id)
+    return jdict_ds 
+
+def tracks2version(tracks):
+    jdict =  _get_dsdict(tracks)
+
+    while 'REPLACED_BY' in jdict.keys():  # is there a newer version? keep going until we find the most recent
+        ds_id = jdict['REPLACED_BY']
+        warnings.warn(f'\n\n*** Newer version exists, see: http://hdl.handle.net/{ds_id}\n')
+        jdict = id2jdict(ds_id)
+
+    version = jdict['VERSION_NUMBER']
     
-    return dataset_id[0], version_id[0]
+    if 'ERRATA_IDS' in jdict.keys():
+        eid = jdict_ds['ERRATA_IDS']
+        warnings.warn(f'\n\n*** Errata exists, see: https://errata.es-doc.org/static/view.html?uid={eid}\n')
 
+    return version, jdict
+
+def tracks2source(tracks):
+    (version, jdict) = tracks2version(tracks)
+    return jdict_ds2source(jdict)
+
+def dsid2source(dsid):
+    jdict = id2jdict(dsid)
+    return jdict_ds2source(jdict)
+
+def jdict_ds2source(jdict_ds):
+    surls = []
+    for track in jdict_ds['HAS_PARTS'].split(';'):
+        jdict_file = id2jdict(track)
+        keys = jdict_file.keys()
+        if 'URL_ORIGINAL_DATA' in keys:        
+            url = jdict_file['URL_ORIGINAL_DATA'].split('"')[1]
+            surls += [url]
+        elif 'URL_REPLICA' in keys:
+            url = jdict_file['URL_REPLICA'].split('"')[1]
+            surls += [url]
+    return sorted(surls) 
